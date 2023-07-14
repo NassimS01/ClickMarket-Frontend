44a9f675783c6f330ca170b8ad11e650e79c6c4e
nassims01 <nassimsalomon01@gmail.com>
2023-07-14 20:14:02 -0300

    error en los import de reactDOM, y react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-
-import { ButtonGlobal } from "./components/ButtonGlobal/ButtonGlobal";
-import CardComponent, { Card, ContainerCards } from "./components/Card/Card";
 import Footer from "./components/footer";
-import { ButtonGlobal } from "./components/GlobalButton/GlobalButton";
 import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
-      <Footer></Footer>  
+      <Footer></Footer>
       <GlobalStyles />
     </>
   );
