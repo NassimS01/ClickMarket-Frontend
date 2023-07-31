c791d4cd6af041e7cdaa88b74abb32d691f18e50
Francisco <JoseFranciscoPaez1999@gmail.com>
2023-07-31 17:21:26 -0300

    commit porque no me deja hacer pull

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@
 // import NavBar from "./components/NavBar/NavBar";
 // import NotFound from "./pages/NotFound/NotFound";
 // import Footer from "./components/footer";
-
+// import Router from "./router/Router";
 import GlobalStyles from "./styles/GlobalStyles";
+
 
 function App() {
   return (
     <>
+      {/* <Router /> */}
+      {/* <Header />
+      <Slider/>
+      <Footer/> */}
       <GlobalStyles />
     </>
   );
