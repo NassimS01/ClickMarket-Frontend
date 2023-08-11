941c6d218ada1b2cfb6542fd8fe4569ff04fb6b3
nassims01 <nassimsalomon01@gmail.com>
2023-08-10 22:30:51 -0300

    merge solucionado

--- src/pages/Category/CategoryStyles.js
+++
@@ -1,7 +0,0 @@
-import { styled } from "styled-components";
-
-export const SectionCategory = styled.div`
-  margin-top: 50px;
-  display: flex;
-  
-`
--- src/utils/formatPrice.js
+++
@@ -1,6 +0,0 @@
-export const formatPrice = (price) => {
-  return new Intl.NumberFormat("es-Ar", {
-    style: "currency",
-    currency: "ARS",
-  }).format(price);
-};
